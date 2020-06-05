7fb0a7e731af920a1f2eafd84e0deaf03350569a
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-06-05 18:00:47 +0900

    エラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
-
-  before_action :configure_permitted_parameters, if: :device_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -11,5 +10,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-  
+
 end
